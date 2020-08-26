c7eecf7ba309b44ddc965dbe0023ca157982e060
Jason Belgard <belgard.jason@gmail.com>
2020-08-25 22:54:04 -0500

    Added color change and speed change

--- src/App.js
+++ src/App.js
@@ -1,26 +1,178 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useRef } from 'react';
+import produce from "immer";
+import "./App.css";
 
-function App() {
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, 1],
+  [1, -1],
+  [-1, -1],
+  [-1, 0],
+  [1, 0],
+]
+
+const numRows = 50;
+const numCols = 50;
+
+const App = () => {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    return rows;
+  });
+
+  const makeEmptyGrid = () => {
+    const rows = []
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    return rows;
+  };
+
+  const [running, setRunning] = useState(false);
+
+  const runningRef = useRef(running);
+  runningRef.current = running
+
+  const speeds = {med : 100, fast : 10, slow : 1000}
+  const [speed, setSpeed] = useState(100)
+
+  const speedRef = useRef(speed)
+  speedRef.current = speed
+
+  console.log("Speed", speed)
+  
+  const runSimulation = useCallback(() => {
+    if(!runningRef.current) {
+      return;
+    }
+
+    setGrid((g) => {
+      return produce(g, gridCopy => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >=0 && newK < numCols) {
+                neighbors += g[newI][newK]
+              }
+            });
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0;              
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1;
+            }
+          }
+        }
+      });
+    });
+    setTimeout(runSimulation, speeds[speedRef.current]);
+  }, [])
+
+  const [color, setColor] = useState ('red')
+  const colors = ['red', 'orange', 'yellow', 'yellowgreen', 'blue', 'white', 'magenta', 'cyan', 'chartreuse', 'black']
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <button
+        onClick = {() => {
+          setRunning(!running);
+          if(!running) {
+            runningRef.current = true;
+            runSimulation()
+          }
+        }}
+      >
+        {running ? "stop" : "start"}
+      </button>
+      <button
+        onClick = {() => {
+          setGrid(makeEmptyGrid)
+        }}
+      >
+        Clear Grid
+      </button>
+      <button
+        onClick = {() => {
+          const rows = [];
+          for (let i = 0; i < numRows; i++) {
+            rows.push(Array.from(Array(numCols), () => Math.random() > 0.5 ? 1 : 0));
+          }
+          setGrid(rows);
+        }}
+      >
+        Random
+      </button>
 
-export default App;
+      <form type = "submit"
+        className = 'colors'
+      >
+        <label htmlFor = "color">
+        <h4 style = {{color: `${color}`}}>Color</h4>
+          <select
+            id = 'type'
+            value = {color}
+            onChange = {e => setColor(e.target.value)}
+          >
+            <option style = {{color: `${color}`}}> {color.color}</option>
+            {colors.map (color => (
+              <option style = {{backgroundColor: `${color}` }}key = {color} value = {color}>
+                {color}
+              </option>
+            ))}
+          </select>
+        </label>
+      </form>
+
+      <form type = 'submit'
+        className = 'speedForm'
+      >
+        <label htmlFor = "speed">
+          <h3>Speed</h3>
+          <select
+            id = 'type'
+            value = {speed}
+            onChange = {e => setSpeed(e.target.value)}
+          >
+            <option value = {speed.med}>Med</option>
+            <option value = {speed.fast}>Fast</option>
+            <option value = {speed.slow}>Slow</option>
+          </select>
+        </label>
+      </form>
+
+      <div
+        style = {{
+          display: "grid",
+          gridTemplateColumns: `repeat(${numCols}, 20px)`,          
+        }}
+      >
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <div
+              key = {`${i} - ${k}`}
+              onClick = {() => {
+                const newGrid = produce(grid, (gridCopy) => {
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                });
+                setGrid(newGrid);
+              }}
+              style = {{
+                width: 20,
+                height: 20,
+                backgroundColor: grid[i][k] ? `${color}` : undefined,
+                border: "solid 1px black",
+              }}
+            />            
+          )))}
+      </div>
+    </>
+  );
+};
+
+export default App
